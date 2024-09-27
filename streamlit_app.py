8790269daaceeb8b0e481a1f921d40bfb07998e6
Pedro Falcão <77029915+falcaovba@users.noreply.github.com>
2024-09-27 08:06:18 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-from datetime import datetime
 
 st.set_page_config(page_title='Acompanhamento de Glicemia - Levy Batista', page_icon='🔬',layout="wide",initial_sidebar_state="collapsed")
 
